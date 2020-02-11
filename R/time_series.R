0ea5d7974c3783800d7432b2c88713ca10aa7480
Martin Becker <martin.becker@mx.uni-saarland.de>
2020-02-11 13:52:22 +0100

    rework of get_timeseries

--- R/time_series.R
+++ R/time_series.R
@@ -32,6 +32,9 @@
 #' for the security
 #' The default value is False
 #' Remark: This parameter has a less precedence than the parameter rawOutput i.e. if rawOutput is set to True, the returned data will be the raw data and this parameter will be ignored
+#' @param convert boolean. If set to \code{TRUE}, the columns of the \code{data.frame}
+#' are converted to appropriate \code{R} data types (instead of \code{character}).
+#' (Currently) defaults to \code{FALSE}.
 #' @param debug boolean
 #' When set to True, the json request and response are printed.
 #'
@@ -43,98 +46,89 @@
 #' @export get_timeseries
 get_timeseries <- function(rics,fields='*', start_date=NULL, end_date=NULL, 
                    interval='daily', normalize=FALSE, count=NULL,
-                   calendar=NULL, corax=NULL,raw_output=FALSE, debug=FALSE) {
+                   calendar=NULL, corax=NULL,raw_output=FALSE, convert=FALSE,
+                   debug=FALSE) {
 
-  Calendar_Values <- list('native', 'tradingdays', 'calendardays')
-  Corax_Values <- list('adjusted', 'unadjusted')
+  Calendar_Values <- c('native', 'tradingdays', 'calendardays')
+  Corax_Values    <- c('adjusted', 'unadjusted')
+  Interval_Values <- c('daily','hour','minute','weekly','monthly','quarterly',
+                       'yearly','tick','taq')
 
   if (is.character(rics)) rics <- list(trimws(rics))
 
   fields <- if ( '*' %in% fields) list('*') else 
     if ('TIMESTAMP' %in% fields) fields else  c('TIMESTAMP',fields)
 
-  if (!is.character(interval)) {  
-    warning('get_timeseries error: The interval parameter should be of character string')
-    return(NULL)
-  }
-
-  # set start_date / end_date in the payload
   if (is.null(start_date)) 
-    start_date <- paste0(as.character(Sys.Date()-100),"T00:00:00")
+    start_date <- paste0(as.character(Sys.Date()-100),"T00:00:00") else
+    start_date <- fixDate(start_date,start=TRUE)
 
   if (is.null(end_date)) 
-    end_date <- paste0(as.character(Sys.Date()),"T23:59:59")
+    end_date <- paste0(as.character(Sys.Date()),"T23:59:59") else
+    end_date <- fixDate(end_date,start=FALSE)
 
-  payload <- list('rics'= rics, 'fields'= fields, 'interval'= interval, 
+  payload <- list('rics'= rics, 'fields'= fields, 
                   'startdate' = start_date,'enddate'= end_date)
 
   count <- sanitize_integer(count,"count")
   if (!is.null(count)) payload$count <- count
 
+  interval <- sanitize_string(interval,"interval")
+  if (!is.null(interval)) {
+		interval <- match.arg(interval,Interval_Values)  
+		payload$interval <- interval
+  }
+
   calendar <- sanitize_string(calendar,"calendar")
-  if (!is.null(calendar)) payload$calendar <- calendar
+  if (!is.null(calendar)) {
+    calendar <- match.arg(calendar,Calendar_Values)
+    payload$calendar <- calendar
+  }
   
   corax <- sanitize_string(corax,"corax")
-  if (!is.null(corax)) payload$corax <- corax
-
+  if (!is.null(corax)) {
+    corax <- match.arg(corax,Corax_Values)
+    payload$corax <- corax
+  }
+  
   json_data = send_json_request("TimeSeries", payload, debug=debug)
 
-  if (raw_output) json_data else {
-    data = jsonlite::fromJSON(json_data)
-    if (normalize) get_normalized_data_frame(data) else
-      get_formatted_data_frame(data)
-  }
+  if (raw_output) json_data else 
+    getFormatted(jsonlite::fromJSON(json_data),convert,normalize)
 }
 
-#'@importFrom utils stack
-get_normalized_data_frame <- function(data)
-{
-  input_data_frame = data$timeseriesData
-  data_frames <- list()
-  for (i in 1:nrow(input_data_frame))
-  { current_row = input_data_frame[i,]
-      fields  = current_row$fields[[1]]$name
-      data_frame  = as.data.frame(current_row$dataPoints[[1]],stringsAsFactors = FALSE)
-      names(data_frame) <- fields
-      data_fields_count = length(fields) - 1 # do not account for TIMESTAMP field
-      timestamps = data_frame[,"TIMESTAMP"]
-      timestamps_column = rep(timestamps,data_fields_count)
-      normalized_frame <- data.frame(Date=timestamps_column)
-      ric = current_row$ric
-      ric_column = rep(ric,nrow(data_frame))
-      ric_column = rep(ric_column,length(fields) - 1)
-      normalized_frame <- cbind(normalized_frame, Security=ric_column)
-      values_columns = data_frame[,!(names(data_frame) %in% c('TIMESTAMP'))]
-      values_columns = stack(values_columns)
-      values_columns = data.frame(Field = values_columns[,2],Value=values_columns[,1])
-      normalized_frame <- cbind(normalized_frame, values_columns)
-      data_frames[[i]] = normalized_frame
-
+#' @importFrom reshape2 melt
+getFormatted <- function(dat,convert=FALSE,normalize=FALSE) {
+  res <- NULL
+  if (names(dat)=="timeseriesData") dat <- dat[[1]]
+  for (i in seq_along(dat$dataPoints)) {
+    if (dat$statusCode[i]!="Normal") 
+      warning(paste0("Status code: ",dat$statusCode[i]," for RIC ",dat$ric[i]))
+    tmp <- as.data.frame(dat$dataPoints[[i]],stringsAsFactors=FALSE)
+    if (normalize) {
+      colnames(tmp) <- dat$fields[[i]]$name
+      tmp <- reshape2::melt(tmp,id.vars=c("TIMESTAMP"))
+      tmp <- tmp[order(tmp[,1]),]
+    }
+    res <- rbind(res,cbind(tmp,RIC=dat$ric[i],stringsAsFactors=FALSE))
   }
-
-  combined_data_frame = do.call("rbind", data_frames)
-  combined_data_frame = combined_data_frame[order( combined_data_frame[,'Security'],combined_data_frame[,'Date']) ,]
-  rownames(combined_data_frame) <- NULL#seq(length=nrow(combined_data_frame))
-  return (combined_data_frame)
-
-}
-
-
-get_formatted_data_frame  <- function(data)
-{
-
-  input_data_frame = data$timeseriesData
-  data_frames <- list()
-  for (i in 1:nrow(input_data_frame))
-  { current_row = input_data_frame[i,]
-    current_fields  = current_row$fields[[1]]$name
-    ric_column = rep( current_row$ric,nrow(input_data_frame[i,]))
-    data_frame  <- as.data.frame(current_row$dataPoints[[1]],stringsAsFactors = FALSE)
-    data_frame <- cbind(data_frame, Security=ric_column)
-    names(data_frame) <- current_fields
-    data_frames[[i]] = data_frame
+  if (convert) {
+    timestamp <- res[,1]
+    timestamp <- gsub("Z","",timestamp)
+    timestamp <- gsub("T"," ",timestamp)
+    res[,1] <- as.POSIXct(timestamp)
+    res[,ncol(res)] <- as.factor(res[,ncol(res)])
   }
-  return (do.call("rbind", data_frames))
-
+  if (normalize) {
+    res <- res[,c(1,4,2,3)]
+    colnames(res) <- c("Date","Security","Field","Value")
+    res[,4] <- as.numeric(res[,4])
+    if (convert) for (i in 2:3) res[,i] <- as.factor(res[,i])
+  } else {
+    colnames(res) <- c(dat$fields[[1]]$name,"RIC")
+    if (convert) for (i in seq(2,ncol(res)-1)) res[,i] <- as.numeric(res[,i])
+  }
+  rownames(res) <- seq_len(nrow(res))
+  res
 }
 
--- R/util.R
+++ R/util.R
@@ -6,18 +6,28 @@ is_named_list <- function (value) is.list(value) && (!is.null(names(value)))
 is_non_named_list <- function (value) is.list(value) && is.null(names(value))
 
 sanitize_string <- function(value,name) {
-  value <- as.character(value)
-  if (is.character(value)&&(length(value)==1)) value else {
-    warning(paste0(name," must be a string"))
-    NULL
+	if (is.null(value)) NULL else {
+		value <- as.character(value)
+		if (is.character(value)&&(length(value)==1)) value else {
+			warning(paste0(name," must be a string"))
+			NULL
+		}
   }
 }
 
 sanitize_integer <- function(value,name) {
-  value <- as.integer(value)
-  if (is.integer(value)&&(length(value)==1)) value else {
-    warning(paste0(name," must be an integer"))
-    NULL
+	if (is.null(value)) NULL else {
+		value <- as.integer(value)
+		if (is.integer(value)&&(length(value)==1)) value else {
+			warning(paste0(name," must be an integer"))
+			NULL
+		}
   }
+}
+
+fixDate <- function(date,start=TRUE) {
+  date <- sanitize_string(date)
+  if (nchar(date)==10) paste0(date,if (start) "T00:00:00" else "T23:59:59") else
+    date	
 }
 
